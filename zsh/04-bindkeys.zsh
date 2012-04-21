fca8624fc6926f24338a8c2e3231d20458af119e
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2012-04-21 04:37:13 +0200

    updated and cleaned up zsh keybindings

--- zsh/04-bindkeys.zsh
+++ zsh/04-bindkeys.zsh
@@ -1,15 +1,8 @@
-bindkey "\e[1~" beginning-of-line
-bindkey "\e[4~" end-of-line
-bindkey "\e[5~" beginning-of-history
-bindkey "\e[6~" end-of-history
-bindkey "\e[3~" delete-char
-bindkey "\e[2~" quoted-insert
-bindkey "\e[5C" forward-word
-bindkey "\eOc" emacs-forward-word
-bindkey "\e[5D" backward-word
-bindkey "\eOd" emacs-backward-word
-bindkey "\e\e[C" forward-word
-bindkey "\e\e[D" backward-word
+# doesn't work sometimes, if not explicitly defined
+bindkey "^[[1;5C" forward-word
+bindkey "^[[1;5D" backward-word
+bindkey "^[[1;3C" emacs-forward-word
+bindkey "^[[1;3D" emacs-backward-word
 # for rxvt
 bindkey "\e[8~" end-of-line
 bindkey "\e[7~" beginning-of-line
