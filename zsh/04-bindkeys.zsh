9b05d07f7e21f48e0c5ead029b8844556f901817
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2012-05-07 03:26:11 +0200

    Updated zsh keybindings & options

--- zsh/04-bindkeys.zsh
+++ zsh/04-bindkeys.zsh
@@ -1,12 +1,14 @@
-# doesn't work sometimes, if not explicitly defined
-bindkey "^[[1;5C" forward-word
-bindkey "^[[1;5D" backward-word
-bindkey "^[[1;3C" emacs-forward-word
-bindkey "^[[1;3D" emacs-backward-word
-# for rxvt
-bindkey "\e[8~" end-of-line
-bindkey "\e[7~" beginning-of-line
-# for non RH/Debian xterm, can't hurt for RH/Debian xterm
-bindkey "\eOH" beginning-of-line
-bindkey "\eOF" end-of-line
+bindkey -e
 
+bindkey "\e[1~" beginning-of-line # Home
+bindkey "\e[4~" end-of-line # End
+bindkey "\e[5~" beginning-of-history # PageUp
+bindkey "\e[6~" end-of-history # PageDown
+bindkey "\e[2~" quoted-insert # Ins
+bindkey "\e[3~" delete-char # Del
+bindkey "\e[5C" forward-word
+bindkey "\eOc" emacs-forward-word
+bindkey "\e[5D" backward-word
+bindkey "\eOd" emacs-backward-word
+bindkey "\e\e[C" forward-word
+bindkey "\e\e[D" backward-word
