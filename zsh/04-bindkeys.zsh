9b1fc1e13004ed46f2903d6d59d47ac50bd984e4
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2012-06-06 05:19:34 +0200

    added console keybindings  (you never know...)

--- zsh/04-bindkeys.zsh
+++ zsh/04-bindkeys.zsh
@@ -27,4 +27,13 @@ case $TERM in
         bindkey "\e[1;3C" forward-word         # Alt-Right
         bindkey "\e[1;3D" backward-word        # Alt-Left
         ;;
+    linux*)
+        bindkey "\e[1~" beginning-of-line    # Home
+        bindkey "\e[4~" end-of-line          # End
+        bindkey "\e[5~" beginning-of-history # PageUp
+        bindkey "\e[6~" end-of-history       # PageDown
+        bindkey "\e[2~" quoted-insert        # Ins
+        bindkey "\e[3~" delete-char          # Del
+        bindkey "\e[C"  emacs-forward-word   # Ctrl-Right
+        bindkey "\e[D"  emacs-backward-word  # Ctrl-Left
 esac
