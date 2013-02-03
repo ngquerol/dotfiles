b67257b25730b6bf159f77723f2becbd3d57c980
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2013-02-03 16:10:57 +0100

    Nuke Arch-specific aliases

--- zsh/07-os_specific.zsh
+++ zsh/07-os_specific.zsh
@@ -2,7 +2,4 @@
 
 if [ -f /etc/debian_version ]; then
     alias apt-get='sudo apt-get'
-elif [ -f /etc/arch-release ]; then
-    alias pacman='sudo pacman-color'
-    compdef _pacman pacman-color = pacman
 fi
