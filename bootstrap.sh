e7026b9f373e68c46e1ccdc2af26d9a9c9ff096b
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2013-10-14 00:36:34 +0200

    Minor update

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,17 +6,17 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
 echo -e "\nSymlinking dotfiles...\n"
-[ -f $HOME/.zsh ] || ln -sv $SCRIPTPATH/zsh ~/.zsh
-[ -f $HOME/.zshrc ] || ln -sv $SCRIPTPATH/zsh/zshrc ~/.zshrc
-[ -f $HOME/.vim  ] || ln -sv $SCRIPTPATH/vim ~/.vim
-[ -f $HOME/.vimrc  ] || ln -sv $SCRIPTPATH/vimrc ~/.vimrc
-[ -f $HOME/.gvimrc ] || ln -sv $SCRIPTPATH/gvimrc ~/.gvimrc
-[ -f $HOME/.gitconfig ] || ln -sv $SCRIPTPATH/gitconfig ~/.gitconfig
+[ -f $HOME/.zsh ] || ln -sv $SCRIPTPATH/zsh $HOME/.zsh
+[ -f $HOME/.zshrc ] || ln -sv $SCRIPTPATH/zsh/zshrc $HOME/.zshrc
+[ -f $HOME/.vim  ] || ln -sv $SCRIPTPATH/vim $HOME/.vim
+[ -f $HOME/.vimrc  ] || ln -sv $SCRIPTPATH/vimrc $HOME/.vimrc
+[ -f $HOME/.gvimrc ] || ln -sv $SCRIPTPATH/gvimrc $HOME/.gvimrc
+[ -f $HOME/.gitconfig ] || ln -sv $SCRIPTPATH/gitconfig $HOME/.gitconfig
 
 echo -e "\nBootstrapping NeoBundle...\n"
 which git > /dev/null
 if [ $? -eq 0 ]; then
-    git clone git://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
+    git clone git://github.com/Shougo/neobundle.vim.git $HOME/.vim/bundle/neobundle.vim
 else
     echo -e "Git wasn't found, aborting."
 fi
