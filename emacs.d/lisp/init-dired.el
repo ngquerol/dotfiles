6a4a58364a165fe0ae0232892ead94674866c8bb
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2014-10-30 22:15:21 +0100

    Add dired-hide-details-mode

--- emacs.d/lisp/init-dired.el
+++ emacs.d/lisp/init-dired.el
@@ -1,8 +1,10 @@
-;;  Don't show dotfiles in dired by default (M-o to toggle)
+;;  Don't show dotfiles in dired by default (C-x M-o to toggle)
 (require 'dired-x)
-(setq-default dired-omit-files-p t) ; this is buffer-local variable
+(setq-default dired-omit-files-p t) ; this is a buffer-local variable
 (setq dired-omit-files
       (concat dired-omit-files "\\|^\\..+$"))
 
+;; Hide clutter
+(add-hook 'dired-mode-hook 'dired-hide-details-mode)
 
 (provide 'init-dired)
