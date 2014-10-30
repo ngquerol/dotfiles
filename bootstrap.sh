3bc6bff7449b6379070c3abbf3a2102c4a5e0a09
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2014-10-30 19:14:44 +0100

    Whitespace fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,5 +6,5 @@ SCRIPTPATH=$(cd "$(dirname "$0")"; pwd)
 echo -e "\nSymlinking dotfiles...\n"
 [ -f $HOME/.zsh ]       || ln -sv $SCRIPTPATH/zsh $HOME/.zsh
 [ -f $HOME/.zshrc ]     || ln -sv $SCRIPTPATH/zsh/zshrc $HOME/.zshrc
-[ -f $HOME/.emacs.d/ ]  || ln -sv $SCRIPTPATH/emacs.d $HOME/.emacs.d
+[ -f $HOME/.emacs.d ]   || ln -sv $SCRIPTPATH/emacs.d $HOME/.emacs.d
 [ -f $HOME/.gitconfig ] || ln -sv $SCRIPTPATH/gitconfig $HOME/.gitconfig
