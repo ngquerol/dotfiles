d3dfe11353858bad7622e4c192f7aa6be410de96
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2014-11-16 12:58:11 +0100

    Remove popwin

--- emacs.d/lisp/init-windows.el
+++ emacs.d/lisp/init-windows.el
@@ -4,12 +4,4 @@
 (global-set-key (kbd "C-c <up>") 'windmove-up)
 (global-set-key (kbd "C-c <down>") 'windmove-down)
 
-;; Popwin
-(require-package 'popwin)
-(require 'popwin)
-(popwin-mode t)
-(push '("*eshell*" :height 10 :position bottom :stick t) popwin:special-display-config)
-(push '("*Backtrace*") popwin:special-display-config)
-(push '("*Apropos*" :height 15 :position bottom :stick t) popwin:special-display-config)
-
 (provide 'init-windows)
