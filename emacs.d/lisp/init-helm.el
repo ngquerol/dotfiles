10dc490c0ed34c2015faadb9d1ebe0cdb17fec8d
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2014-11-19 19:14:04 +0100

    Add helm-recentf keybinding

--- emacs.d/lisp/init-helm.el
+++ emacs.d/lisp/init-helm.el
@@ -6,6 +6,7 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x C-r") 'helm-recentf)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-c h g") 'helm-google-suggest)
 
