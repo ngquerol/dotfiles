e50ac4f33cdf9bdd5ac9e8283c5cb7eca8237629
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2014-11-26 19:34:50 +0100

    Revamp eshell config

--- emacs.d/lisp/init-eshell.el
+++ emacs.d/lisp/init-eshell.el
@@ -1,4 +1,25 @@
-;; Clear eshell buffer like in bash or zsh
+(defun eshell-here ()
+  "Opens up a new shell in the directory associated with the
+current buffer's file. The eshell is renamed to match that
+directory to make multiple eshell windows easier."
+  (interactive)
+  (let* ((parent (if (buffer-file-name)
+                     (file-name-directory (buffer-file-name))
+                   default-directory))
+         (height (/ (window-total-height) 3))
+         (name   (car (last (split-string parent "/" t)))))
+    (split-window-vertically (- height))
+    (other-window 1)
+    (eshell "new")
+    (rename-buffer (concat "*eshell: " name "*"))))
+
+(defun eshell/x ()
+  "exit eshell and delete its window"
+  (interactive)
+  (insert "exit")
+  (eshell-send-input)
+  (delete-window))
+
 (defun eshell/clear ()
   "clear the eshell buffer."
   (interactive)
@@ -6,11 +27,12 @@
     (erase-buffer)
     (eshell-send-input)))
 
-(add-hook 'eshell-mode-hook
-          '(lambda ()
-             (local-set-key (kbd "C-l") 'eshell/clear)
-             (local-set-key (kbd "C-d") 'eshell-life-is-too-much)))
+(setq eshell-banner-message "\n")
 
-(global-set-key (kbd "C-c s") 'eshell)
+(add-hook 'eshell-mode-hook #'(lambda ()
+                                (define-key eshell-mode-map (kbd "C-d") 'eshell/x)
+                                (define-key eshell-mode-map (kbd "C-l") 'eshell/clear)))
+
+(global-set-key (kbd "C-c s") 'eshell-here)
 
 (provide 'init-eshell)
