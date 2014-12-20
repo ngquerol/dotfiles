b0224a10173b2885858e5c05063a1ffc9232739c
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2014-12-20 19:27:51 +0100

    Add fix for PATH

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -34,3 +34,11 @@
 
 ;; Finally, set default locales
 (require 'init-locales)
+
+;; Fix the PATH variable
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(if window-system (set-exec-path-from-shell-PATH))
