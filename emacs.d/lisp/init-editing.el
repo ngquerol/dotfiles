45e83d6a816c53fefb744259dbe58414ace47a3b
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2014-12-20 19:28:23 +0100

    Add a truckload of editing conveniences, flycheck

--- emacs.d/lisp/init-editing.el
+++ emacs.d/lisp/init-editing.el
@@ -39,6 +39,39 @@
 ;; Automagically indent when inserting a newline
 (global-set-key (kbd "RET") 'reindent-then-newline-and-indent)
 
+;; Faster repeat
+(global-set-key (kbd "C-z") 'repeat)
+
+;; Join line
+(defun my-join-line ()
+  (interactive)
+  (join-line -1))
+
+(global-set-key (kbd "M-j") 'my-join-line)
+
+;; Smart open line
+(defun smart-open-line ()
+  "Insert an empty line after the current line.
+Position the cursor at its beginning, according to the current mode."
+  (interactive)
+  (move-end-of-line nil)
+  (newline-and-indent))
+
+(defun smart-open-line-above ()
+  "Insert an empty line above the current line.
+Position the cursor at it's beginning, according to the current mode."
+  (interactive)
+  (move-beginning-of-line nil)
+  (newline-and-indent)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+(global-set-key (kbd "M-<down>") 'smart-open-line)
+(global-set-key (kbd "M-<up>") 'smart-open-line-above)
+
+;; Delete the whole line
+(global-set-key (kbd "C-c d") 'kill-whole-line)
+
 ;; Comment what I really mean
 (defun comment-dwim-line (&optional arg)
   "Replacement for the comment-dwim command.
@@ -54,7 +87,7 @@ comment at the end of the line."
     (comment-dwim arg)))
 (global-set-key (kbd "C-c c") 'comment-dwim-line)
 
-;; Smart home key
+;; Smart beginn
 (defun smart-beginning-of-line ()
   "Move point to first non-whitespace character or beginning-of-line."
   (interactive "^")
@@ -65,6 +98,9 @@ comment at the end of the line."
 (global-set-key (kbd "<home>") 'smart-beginning-of-line)
 (global-set-key (kbd "C-a") 'smart-beginning-of-line)
 
+;; Align according to regexp
+(global-set-key (kbd "C-c a") 'align-regexp)
+
 ;; Expand-region
 (require-package 'expand-region)
 (global-set-key (kbd "C-c e") 'er/expand-region)
@@ -72,17 +108,30 @@ comment at the end of the line."
 ;; Company
 (require-package 'company)
 (require-package 'company-c-headers)
-(setq company-minimum-prefix-length 2
-      company-abort-manual-when-too-short t
-      company-selection-wrap-around t
-      company-idle-delay 0.25)
-(add-hook 'after-init-hook 'global-company-mode)
-(add-hook 'c-mode-common-hook
-          '(lambda ()
-             (add-to-list 'company-backends 'company-c-headers)
-             (delete 'company-semantic company-backends)))
+(require-package 'company-go)
+(with-eval-after-load "company"
+  (setq company-minimum-prefix-length 2
+        company-abort-manual-when-too-short t
+        company-selection-wrap-around t
+        company-idle-delay 0.25)
+  (setq company-backends '(company-clang
+                           company-c-headers
+                           company-go
+                           company-capf
+                           company-dabbrev-code
+                           company-files))
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  (define-key company-active-map (kbd "<tab>") 'company-complete))
 
-;; Various keybindings
-(global-set-key (kbd "C-c a") 'align-regexp)
+;; Flycheck
+(with-eval-after-load "flycheck"
+  (setq-default flycheck-gcc-language-standard "c99"
+                flycheck-clang-language-standard "c99"
+                flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
+
+;; Programming-specific modes
+(add-hook 'prog-mode-hook #'flycheck-mode)
+(add-hook 'prog-mode-hook #'company-mode)
 
 (provide 'init-editing)
