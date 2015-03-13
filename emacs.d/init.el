99cdfe414baa1827ed1ef12c10f7369db998f222
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2015-03-13 10:11:16 +0100

    Add useful functions

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -77,6 +77,36 @@
          ("C-c <up>" . windmove-up)
          ("C-c <down>" . windmove-down)))
 
+;; Manipulate buffers and corresponding files
+(defun rename-file-and-buffer ()
+  "Rename the current buffer and file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (message "Buffer is not visiting a file!")
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond
+         ((vc-backend filename) (vc-rename-file filename new-name))
+         (t
+          (rename-file filename new-name t)
+          (set-visited-file-name new-name t t)))))))
+
+(global-set-key (kbd "C-c R") #'rename-file-and-buffer)
+
+(defun delete-file-and-buffer ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (when filename
+      (if (vc-backend filename)
+          (vc-delete-file filename)
+        (progn
+          (delete-file filename)
+          (message "Deleted file %s" filename)
+          (kill-buffer))))))
+
+(global-set-key (kbd "C-c D")  'delete-file-and-buffer)
+
 ;;; Editing
 
 (setq-default auto-save-default nil
