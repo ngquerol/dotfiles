32e8ee23ea3fee745cdde5866ea16c15e3038740
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2015-09-02 13:32:19 +0200

    Remove killall completion

--- zsh/06-zstyle.zsh
+++ zsh/06-zstyle.zsh
@@ -28,8 +28,6 @@ zstyle ':completion:*:functions' ignored-patterns '_*'
 # PID completion
 zstyle ':completion:*:processes' command ps -A -o pid,user,command
 zstyle ':completion:*:processes-names' command 'ps c -u ${USER} -o command | uniq'
-zstyle ':completion:*:(killall|pkill|kill):*' menu yes select
-zstyle ':completion:*:(killall|pkill|kill):*' force-list always
 zstyle ':completion:*:*:kill:*:processes' list-colors "=(#b) #([0-9]#)*=0=01;31"
 
 # Sudo completion
@@ -42,8 +40,8 @@ zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' get-revision true
 zstyle ':vcs_info:*' stagedstr '%F{green}*%f '
 zstyle ':vcs_info:*' unstagedstr '%F{yellow}*%f '
-zstyle ':vcs_info:git*' formats '%F{cyan}± %b%f %F{white}%7.7i%f %m%c%u'
-zstyle ':vcs_info:git*' actionformats '%F{cyan}± %b%f %F{blue}[%a]%f %F{white}%7.7i%f %m%c%u'
+zstyle ':vcs_info:git*' formats '%F{cyan}± %b%f %F{white}%.7i%f %m%c%u'
+zstyle ':vcs_info:git*' actionformats '%F{cyan}± %b%f %F{blue}[%a]%f %F{white}%(!.%.7i.)%f %m%c%u'
 zstyle ':vcs_info:git*+set-message:*' hooks git-untracked git-aheadbehind
 zstyle ':vcs_info:svn*' formats '%F{cyan}%s %r%f %F{white}r%i%f %m%c%u'
 
