fb84f852cc8fc6797c5e19534ebbe5db1a4a0467
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2015-09-19 17:14:30 +0200

    Properly quote some variables

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env sh
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $(basename "$0") <name of backup> <dir to backup>"
+if [ ${#} -ne 2 ]; then
+    echo "Usage: $(basename "${0}") <name of backup> <dir to backup>"
     exit 1
 fi
 
-backup_file_name=$1
-backup_src_dir=$2
-backup_dest_dir=~/Dropbox/backups
-backup_dest_file=$backup_dest_dir/$backup_file_name.tar.xz
-exclude_file=.exclude
+backup_file_name="${1}"
+backup_src_dir="${2}"
+backup_dest_dir="${HOME}/Dropbox/backups"
+backup_dest_file="${backup_dest_dir}/${backup_file_name}.tar.xz"
+exclude_file=".exclude"
 
-if [ ! -d "$backup_src_dir" ]; then
+if [ ! -d "${backup_src_dir}" ]; then
     echo "\"${backup_src_dir}\" is not a directory or does not exist."
     exit 1
 fi
 
-if [ -f "$backup_dest_file" ]; then
+if [ -f "${backup_dest_file}" ]; then
     while true; do
         read -p "${backup_dest_file} already exists, overwrite? (y/n) " answer
         case $answer in
@@ -27,14 +27,14 @@ if [ -f "$backup_dest_file" ]; then
     done
 fi
 
-echo "Backing up \"$backup_src_dir\" to \"$backup_dest_file\"..."
+echo "Backing up \"${backup_src_dir}\" to \"${backup_dest_file}\"..."
 
-cd "$backup_src_dir"
+cd "${backup_src_dir}"
 
-if [ -f "$exclude_file" ]; then
-    tar --exclude-from "$exclude_file" -cf "$backup_dest_file" * 1>/dev/null 2>&1
+if [ -f "${exclude_file}" ]; then
+    tar --exclude-from "${exclude_file}" -cf "${backup_dest_file}" * 1>/dev/null
 else
-    tar -cf "$backup_dest_file" * 1>/dev/null 2>&1
+    tar -cf "${backup_dest_file}" * 1>/dev/null
 fi
 
 tar=$?
