c401c476817b985576d2672147b2ae57ca37e464
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2017-02-09 11:11:42 +0100

    backup.sh: Use multithreaded LZMA compression, cleanups

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env sh
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
 if [ ${#} -ne 2 ]; then
     echo "Usage: $(basename "${0}") <name of backup> <dir to backup>"
     exit 1
 fi
 
-tar_options="cJf"
+compression_program="xz --threads=0"
 backup_file_name="${1}"
 backup_src_dir="${2}"
 backup_dest_dir="${HOME}/Library/Mobile Documents/com~apple~CloudDocs/backups"
 backup_dest_file="${backup_dest_dir}/${backup_file_name}.tar.xz"
-exclude_file=".exclude"
+exclude_file="${backup_src_dir}/.exclude"
+
+if [ ! -d "${backup_src_dir}" ]; then
+    echo "Directory \"${backup_src_dir}\" does not exists, aborting."
+    exit 1
+fi
+
+echo "Backing up \"${backup_src_dir}\" to \"${backup_dest_file}\"..."
 
 if [ -f "${backup_dest_file}" ]; then
     printf "%s already exists, overwrite? (y/n) " "${backup_dest_file}"
@@ -26,17 +37,14 @@ if [ -f "${backup_dest_file}" ]; then
     done
 fi
 
-echo "Backing up \"${backup_src_dir}\" to \"${backup_dest_file}\"..."
-
-(
-    cd "${backup_src_dir}" || (echo "${backup_src_dir} does not exists!" && exit 1)
-
-    if [ -f $exclude_file ]; then
-        tar ${tar_options} "${backup_dest_file}" --exclude-from ${exclude_file}  .
-    else
-        tar ${tar_options} "${backup_dest_file}" .
-    fi
-)
+if [ -f "${exclude_file}" ]; then
+    tar --exclude-from "${exclude_file}" \
+        -cpf - "${backup_src_dir}" \
+        | ${compression_program} > "${backup_dest_file}"
+else
+    tar -cpf - "${backup_src_dir}" \
+        | ${compression_program} > "${backup_dest_file}"
+fi
 
 tar_exit=$?
 
