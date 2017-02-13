95b6db1745da924cd651546d884954c087f89f7b
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2017-02-13 23:27:32 +0100

    flac2alac.sh: small enhancements, fix error for cover sizes with odd width/height

--- bin/flac2alac.sh
+++ bin/flac2alac.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-shopt -s extglob
-
 set -o errexit
 set -o nounset
 set -o pipefail
@@ -9,75 +7,86 @@ set -o pipefail
 # Function definitions
 check_prerequisites() {
     if ! command -v parallel &>/dev/null; then
-        echo >&2 "GNU Parallel is not available. Aborting.";
-        exit 1;
+        echo >&2 "GNU Parallel is not available. Aborting."
+        exit 1
     fi
 
     if ! command -v AtomicParsley &>/dev/null; then
-        echo >&2 "AtomicParsley is not available. Aborting.";
-        exit 1;
+        echo >&2 "AtomicParsley is not available. Aborting."
+        exit 1
     fi
 
     if ! command -v ffmpeg &>/dev/null; then
-        echo >&2 "FFmpeg is not available. Aborting.";
-        exit 1;
+        echo >&2 "FFmpeg is not available. Aborting."
+        exit 1
     fi
 }
 
 process_dir() {
     local targetdir="${1}"
     local outputdir="${2}"
-    local cover_file="$(ls "${targetdir}"@(cover|folder).@(jpeg|jpg) 2>/dev/null)"
 
-    parallel --bar convert_track {} \'"${cover_file}"\' "${outputdir}" ::: "${targetdir}"*.flac
+    (
+        shopt -s extglob nocaseglob
 
-    rm -f "${targetdir}"cover-resized-*.jpg # nasty AtomicParsley
+        cd "${targetdir}"
+
+        echo -e "\nProcessing \"${PWD##*/}\"..."
+
+        cover_file="$(ls @(cover|folder).@(jpeg|jpg))"
+
+        parallel --bar convert_track {} \'"${cover_file}"\' \'"${outputdir}"\' ::: *.flac
+    )
 }
+export -f process_dir
 
 convert_track() {
     local track="${1}"
     local cover_file="${2}"
     local outputdir="${3}"
     local converted_track="${track%.*}.m4a"
-    local embedded_cover="${track%.*}.jpg"
 
-    ffmpeg -hide_banner -nostats -loglevel panic -i "${track}" -y -c:a alac "${converted_track}" < /dev/null
+    ffmpeg -hide_banner -loglevel error -i "${track}" -y -vf \
+           crop="((in_w/2)*2):((in_h/2)*2)" -c:a alac "${converted_track}"
 
-    if [ ! -z "${cover_file}" ]; then
-        AtomicParsley "${converted_track}" --artwork "${cover_file}" --overWrite 1>/dev/null
-    else
-        if ffprobe -hide_banner -loglevel panic -i "${track}" | grep -q "Stream #0:1: Video"; then
-            ffmpeg -hide_banner -nostats -loglevel panic -i "${track}" -y -vf \
-                   crop="((in_w/2)*2):((in_h/2)*2)" "${embedded_cover}" < /dev/null
-            AtomicParsley "${converted_track}" --artwork "${embedded_cover}" --overWrite 1>/dev/null
-            rm "{$embedded_cover}"
-        fi
+    if [ ! -z "${cover_file:-}" ] && [ -f "${cover_file}" ]; then
+        AtomicParsley "${converted_track}" --artwork "${cover_file}" \
+                      --overWrite 1>/dev/null
+    elif ffprobe -hide_banner -loglevel error -i "${track}" | grep -q "Stream #0:1: Video"; then
+        local embedded_cover="${track%.*}.jpg"
+
+        ffmpeg -hide_banner -loglevel error -i "${track}" -y -vf \
+               crop="((in_w/2)*2):((in_h/2)*2)" "${embedded_cover}"
+        AtomicParsley "${converted_track}" --artwork "${embedded_cover}" \
+                      --overWrite 1>/dev/null
+        rm "{$embedded_cover}"
     fi
+
+    rm -f "*-resized-*.jpg"
 }
 export -f convert_track
 
 # Start
-if [ ${#} -ne 1 ] && [ ${#} -ne 2 ]; then
+if [ ${#} -lt 1 ] || [ ${#} -gt 2 ]; then
     echo "Usage: flac2alac.sh <source dir> [<target dir>]"
     exit 1
 fi
 
-# TODO: not compatible with relative paths starting with ".." or "~"
-# Alas there is no readlink/realpath binaries installed by default on OSX.
+check_prerequisites
+
 if [ -d "${1}" ]; then
-    sourcedir=$([[ ${1} = /* ]] && echo "${1}" || echo "${PWD}/${1#./}")
+    sourcedir=$(cd "${1}" && pwd)
 else
     echo -e >&2 "Could not resolve source directory ${1}. Aborting."
     exit 1
 fi
 
-if [ -d "$2" ]; then
-    outputdir=$([[ ${2} = /* ]] && echo "${2}" || echo "${PWD}/${2#./}")
+if [ ! -z "${2:-}" ] && [ -d "$2" ]; then
+    outputdir=$(cd "${2}" && pwd)
 else
+    echo "Invalid or unspecified output directory; processing input files in-place."
     outputdir=${sourcedir}
 fi
-
-check_prerequisites
 
 # TODO: find a more efficient solution (check once for *.flac per directory)
 # Ugly array syntax courtesy of Bash 3.
@@ -89,10 +98,9 @@ done < <(find "${sourcedir}" -type f -iname "*.flac" | grep -o '.*/' | sort -u)
 targetdirs_count=${#targetdirs[@]}
 
 if [ "${targetdirs_count}" -ne 0 ]; then
-    echo -e "\nFound ${targetdirs_count} target(s) containing FLAC files."
+    echo -e "\nFound ${targetdirs_count} directories containing FLAC files."
 
     for targetdir in "${targetdirs[@]}"; do
-        echo -e "\nProcessing directory ${targetdir}..."
         process_dir "${targetdir}" "${outputdir}"
     done
 
