b12a479020cbb2dd0ec52ead5a6ff7e5b4df8260
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2017-06-12 21:51:58 +0200

    Fix zsh prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -9,7 +9,7 @@ zstyle ":vcs_info:git*" formats "%F{cyan}± %b%f %F{white}%.7i%f %m%c%u"
 zstyle ":vcs_info:git*" actionformats "%F{cyan}± %b%f %F{blue}[%a]%f %F{white}%(!.%.7i.)%f %m%c%u"
 zstyle ":vcs_info:git*+set-message:*" hooks git-untracked git-aheadbehind git-remotebranch
 
-PROMPT='%B%F{green}%50<...<%3~%f ${vcs_info_msg_0_}→%b '
+PROMPT='%B%F{green}%(5~|%-1~/…/%3~|%4~)%f ${vcs_info_msg_0_}→%b '
 
 [ -n "${SSH_CLIENT}" ] && PROMPT='%B%F{yellow}⚡%f %F{blue}%n@%m%f%b ${PROMPT}'
 
