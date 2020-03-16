25b8a441bd55f01a8aa6d1dab254238a66603fb5
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2020-03-16 20:59:20 +0100

    zsh: Show git rebase progress, fix RPROMPT spacing

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,9 +1,13 @@
 ## prompt customization
 
-## vcs information retrieval
+autoload -Uz add-zsh-hook
+
+# do not put a space after right prompt
+ZLE_RPROMPT_INDENT=0
+
+## VCS information retrieval
 
 autoload -Uz vcs_info
-autoload -Uz add-zsh-hook
 
 add-zsh-hook precmd vcs_info
 
@@ -12,9 +16,10 @@ zstyle ":vcs_info:*" check-for-changes true
 zstyle ":vcs_info:*" get-revision true
 zstyle ":vcs_info:*" stagedstr "%F{green}*%f"
 zstyle ":vcs_info:*" unstagedstr "%F{yellow}*%f"
-zstyle ":vcs_info:git*" formats "%F{cyan}± %b%f %F{white}%.7i%f %m%c%u"
-zstyle ":vcs_info:git*" actionformats "%F{cyan}± %b%f %F{yellow}(%a)%f"
-zstyle ":vcs_info:git*+set-message:*" hooks git-untracked git-aheadbehind git-remotebranch git-stash
+zstyle ":vcs_info:git:*" patch-format "%F{magenta}(%n/%a)%f %.7p"
+zstyle ":vcs_info:git:*" formats "%F{cyan}± %b%f %.7i%m%c%u"
+zstyle ":vcs_info:git:*" actionformats "%F{cyan}± %b%f %F{magenta}%a%f%m%c%u"
+zstyle ":vcs_info:git*+set-message:*" hooks git-untracked git-aheadbehind git-remotebranch git-stash git-message 
 
 # show an indicator if there are untracked files
 function +vi-git-untracked() {
@@ -30,13 +35,17 @@ function +vi-git-aheadbehind() {
     local ahead behind
     local -a gitstatus
 
+    if [ -d $(git rev-parse --git-path rebase-merge) ]; then
+        return
+    fi
+
     behind=$(git rev-list HEAD..${hook_com[branch]}@{upstream} 2>/dev/null | grep -ch "^")
-    (( $behind )) && gitstatus+=( "%F{red}↓%f${behind} " )
+    (( $behind )) && gitstatus+=" %F{red}↓%f${behind}"
 
     ahead=$(git rev-list ${hook_com[branch]}@{upstream}..HEAD 2>/dev/null | grep -ch "^")
-    (( $ahead )) && gitstatus+=( "%F{green}↑%f${ahead} " )
+    (( $ahead )) && gitstatus+=" %F{green}↑%f${ahead}"
 
-    hook_com[misc]+=${gitstatus}
+    hook_com[misc]+=${(j..)gitstatus}
 }
 
 # show the name of the remote branch if it differs from the local one
@@ -47,7 +56,7 @@ function +vi-git-remotebranch() {
                    --symbolic-full-name 2>/dev/null)/refs\/remotes\/}
 
     if [[ -n ${remote} && ${remote#*/} != ${hook_com[branch]} ]]; then
-        hook_com[branch]="${hook_com[branch]} [${remote}]"
+        hook_com[branch]+=" [${remote}]"
     fi
 }
 
@@ -60,9 +69,18 @@ function +vi-git-stash() {
     local -a stashes=(${(f)"$(<${vcs_comm[gitdir]}/logs/refs/stash)"})
 
     if [ ${#stashes} -gt 0 ]; then
-        hook_com[misc]+="%F{magenta}$%f"
+        hook_com[misc]+=" %F{magenta}${#stashes}$%f"
     fi
 }
+
+# proper misc & (un)staged spacing
+function +vi-git-message() {
+    if [[ -n ${hook_com[staged]} || -n ${hook_com[unstaged]} ]]; then
+        hook_com[misc]+=" "
+    fi
+}
+
+## prompt rendering functions
 
 # print a newline before the prompt, unless it's the first prompt in the process
 function print_newline() {
