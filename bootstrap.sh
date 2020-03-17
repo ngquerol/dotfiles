d9716c2fff55f7387ac44c790db6d1521ec639e4
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2020-03-17 21:00:00 +0100

    Modernize bootstrap script
    - Require bash >= 3.2 instead of sh
    - Add options to configure verbosity & clobbering

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,108 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -o errexit
 set -o nounset
 
+## Global variables
+
+# Dotfiles (regular files or directories) to manage
+dotfiles=(
+    "zsh"
+    "zshrc"
+    "zprofile"
+    "zprofile.local"
+    "bin"
+    "gitconfig"
+    "emacs.d"
+    "vimrc"
+)
+
 # Absolute path this script is in
 script_path=$(cd "$(dirname "${0}")" && pwd)
 
-symlink() {
-    source="${1}"
-    target="${HOME}/.$(basename "${1}")"
+## Functions
 
-    ln -Fnsv "${source}" "${target}"
+link_target() {
+    local dotfile
+    dotfile=${1}
+
+    echo "${HOME}/.${dotfile}"
 }
 
-printf "Symlinking files...\\n"
+unlink() {
+    local target
+    target="$(link_target "${1}")"
 
-symlink "${script_path}/zsh"
-symlink "${script_path}/zshrc"
-symlink "${script_path}/zprofile"
-symlink "${script_path}/zprofile.local"
-symlink "${script_path}/bin"
-symlink "${script_path}/gitconfig"
-symlink "${script_path}/emacs.d"
-symlink "${script_path}/vimrc"
+    if [ -L "${target}" ]; then
+        [ -n "${verbose_opt}" ] && echo "Removing symlink \"${target}\""
+        rm "${target}"
+    fi
+}
 
-printf "Done.\\n"
+link() {
+    local target
+    target="$(link_target "${1}")"
+
+    [ -n "${verbose_opt}" ] && printf "\"%s\" => \"%s\"" \
+                                      "${dotfile}" "${target}"
+
+    if [ -n "${clobber_opt}" ]; then
+        if [ -n "${verbose_opt}" ] && [ -e "${target}" ]; then
+            printf " (clobbering existing target)"
+        fi
+
+        ln -Fns "${script_path}/${dotfile}" "${target}"
+    else
+        if [ -n "${verbose_opt}" ] && [ -e "${target}" ]; then
+            printf "\rSkipping \"%s\": target \"%s\" already exists." \
+                   "${dotfile}" "${target}"
+            return
+        fi
+
+        ln -ns "${script_path}/${dotfile}" "${target}"
+    fi
+
+    [ -n "${verbose_opt}" ] && printf "\n"
+}
+
+print_usage() {
+    echo "usage: ${0} [-(-l)ink|-(-u)nlink] (-(-v)erbose) (-(-f)orce)"
+}
+
+## Entry point
+
+link_opt=
+unlink_opt=
+clobber_opt=
+verbose_opt=
+
+while (( $# )); do
+    case ${1} in
+        -l|--link) [ -n "${unlink_opt}" ] && \
+                       { print_usage >&2; exit 1; } || link_opt=1 ;;
+        -u|--unlink) [ -n "${link_opt}" ] && \
+                         { print_usage >&2; exit 1; } || unlink_opt=1 ;;
+        -f|--force) clobber_opt=1 ;;
+        -v|--verbose) verbose_opt=1 ;;
+        *) print_usage >&2; exit 1 ;;
+    esac
+
+    shift
+done
+
+if [ -z "$link_opt" ] && [ -z "$unlink_opt" ]; then
+    print_usage >&2
+    exit 1
+fi
+
+if [ -n "$link_opt" ]; then
+    echo "Symlinking dotfiles..."
+    for dotfile in "${dotfiles[@]}"; do link "${dotfile}"; done
+    echo "Done."
+fi
+
+if [ -n "$unlink_opt" ]; then
+    echo "Unlinking dotfiles..."
+    for dotfile in "${dotfiles[@]}"; do unlink "${dotfile}"; done
+    echo "Done."
+fi
