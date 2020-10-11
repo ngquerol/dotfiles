565178cbade8f325cfbf8d2750a93141b20f6996
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2020-10-12 00:50:19 +0200

    Add simple tmux config, with companion script

---
+++ bin/tmux_system_stats.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+
+set -u
+set -e
+
+pgrep "tmux" 1>/dev/null 2>&1 || { echo "tmux is not running!"; exit 1; }
+
+get_tmux_option() {
+  local option="$1"
+  local default_value="$2"
+  local option_value
+
+  option_value="$(tmux show-option -gqv "$option")"
+
+  if [ -z "$option_value" ]; then
+    echo "$default_value"
+  else
+    echo "$option_value"
+  fi
+}
+
+refresh_interval=$(get_tmux_option "status-interval" "5")
+samples_count="60"
+os="$(uname -s)"
+
+cpu_usage() {
+    case "${os}" in
+        "Darwin")
+            iostat -w "${refresh_interval}" -c "${samples_count}" \
+                | awk 'NR > 2 { printf "cpu: %02d%%\n", 100-$(NF-3) }'
+            ;;
+        "Linux")
+            vmstat -n "$refresh_interval" "$samples_count" \
+                | awk 'NR > 2 { printf "cpu: %02d%%\n", 100-$(NF-2) }'
+            ;;
+        *) echo "??" ;;
+    esac
+}
+
+mem_stats() {
+    case "${os}" in
+        "Darwin")
+            local page_size
+            page_size="$(sysctl -nq "vm.pagesize")"
+
+            vm_stat | awk -v page_size="${page_size}" -F ':' '
+    BEGIN { free=0; used=0 }
+
+    /Pages active/ ||
+    /Pages wired/ {
+      gsub(/^[ \t]+|[ \t]+$/, "", $2); used+=$2;
+    }
+    /Pages free/ ||
+    /Pages inactive/ ||
+    /Pages speculative/ ||
+    /Pages occupied by compressor/ {
+      gsub(/^[ \t]+|[ \t]+$/, "", $2); free+=$2;
+    }
+    END { print (free * page_size)/1024, (used * page_size)/1024 }
+  '
+            ;;
+        "Linux")
+            </proc/meminfo awk '
+    BEGIN { total=0; free=0; }
+      /MemTotal:/ { total=$2; }
+
+      /MemFree:/ { free+=$2; }
+      /Buffers:/ { free+=$2; }
+      /Cached:/ { free+=$2; }
+      /MemAvailable:/ { free=$2; exit;}
+    END { print free, total-free }
+  '
+            ;;
+        *) "??" ;;
+    esac
+}
+
+mem_usage() {
+    mem_stats | awk -v scale="1048576" '{ printf "mem: %.2f/%.2fG", $2/scale, $1/scale + $2/scale }'
+}
+
+load_avg(){
+    uptime | awk '{
+    sub(/,$/, "", $(NF-2));
+    sub(/,$/, "", $(NF-1));
+    sub(/,$/, "", $NF);
+    printf "load: %.2f %.2f %.2f", $(NF-2), $(NF-1), $NF
+  }'
+}
+
+case "${1}" in
+    "cpu") cpu_usage ;;
+    "mem") mem_usage ;;
+    "load") load_avg ;;
+esac
