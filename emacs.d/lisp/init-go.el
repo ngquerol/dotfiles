7b225a007dbdd4a4dde32da60aea2fdd1d44211e
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2020-11-22 21:53:50 +0100

    emacs.d: go-mode updates

--- emacs.d/lisp/init-go.el
+++ emacs.d/lisp/init-go.el
@@ -15,11 +15,24 @@
     (add-hook 'before-save-hook #'lsp-organize-imports t t))
   :mode ("\\.go\\'" . go-mode)
   :config
+  ;; Smartparens integration
+  (with-eval-after-load 'smartparens
+    (sp-local-pair 'go-mode "{" nil :post-handlers '(("||\n[i]" "RET"))))
+
+  ;; Projectile integration
+  (with-eval-after-load 'projectile
+    (add-to-list 'projectile-project-root-files "go.mod"))
 
   ;; LSP integration
   (when (and (package-installed-p 'lsp-mode)
              (executable-find "gopls"))
-    (setq-default lsp-go-hover-kind "FullDocumentation")
+    (require 'lsp)
+    (setq-default lsp-go-hover-kind "FullDocumentation"
+                  lsp-go-link-target "pkg.go.dev")
+    (lsp-register-custom-settings
+     `(("gopls.completeUnimported" t t)
+       ("gopls.staticcheck" ,(if (executable-find "staticcheck") t nil) t)
+       ("gopls.gofumpt" ,(if (executable-find "gofumpt") t nil) t)))
     (add-hook 'go-mode-hook #'ngq/lsp-go-install-save-hooks)
     (add-hook 'go-mode-hook #'lsp-deferred)))
 
