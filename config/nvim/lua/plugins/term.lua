f6451161f09623c5d7110241ec1c197f0b2bced9
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2024-01-27 13:43:24 +0100

    nvim: add lazygit term keymap, fix existing term keymap descriptions
    Ensure the keymap is set only if lazygit is executable.

--- config/nvim/lua/plugins/term.lua
+++ config/nvim/lua/plugins/term.lua
@@ -1,10 +1,23 @@
 return {
 	"akinsho/toggleterm.nvim",
 	keys = {
-		{ "<leader>tf", "<cmd>ToggleTerm direction=float<cr>" },
-		{ "<leader>tv", "<cmd>ToggleTerm direction=vertical<cr>" },
-		{ "<leader>ts", "<cmd>ToggleTerm direction=horizontal<cr>" },
-		{ "<leader>tt", "<cmd>TermSelect<cr>" },
+		{ "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "Open terminal (floating)" },
+		{ "<leader>tv", "<cmd>ToggleTerm direction=vertical<cr>", desc = "Open terminal (vertical)" },
+		{ "<leader>ts", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Open terminal (horizontal)" },
+		{ "<leader>tt", "<cmd>TermSelect<cr>", desc = "Select terminal" },
+		{
+			"<leader>tg",
+			function()
+				if vim.fn.executable("lazygit") ~= 1 then
+					vim.api.nvim_feedkeys("<leader>tg", "n", true)
+				else
+					local Terminal = require("toggleterm.terminal").Terminal
+					local lazygit = Terminal:new({ cmd = "lazygit", silent = true })
+					lazygit:toggle()
+				end
+			end,
+			desc = "Open lazygit (floating)",
+		},
 	},
 	opts = {
 		close_on_exit = true,
@@ -14,7 +27,7 @@ return {
 		persist_size = true,
 		shade_terminals = true,
 		shading_factor = 2,
-		shell = vim.o.shell,
+		shell = vim.fn.executable("fish") == 1 and "fish" or vim.o.shell,
 		start_in_insert = true,
 		size = function(term)
 			if term.direction == "horizontal" then
