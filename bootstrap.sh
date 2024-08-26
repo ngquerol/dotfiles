70c455d2fbabe37046aaf6be52d58aed1b02ff91
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2024-08-26 21:06:26 +0200

    bootstrap: run shfmt

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ script_path="${script_dir_path}/${script_name}"
 # TODO: clobber w/ interactive prompt ?
 
 print_usage() {
-    echo "usage: ${script_name} [(l)ink|(u)nlink] (-(-d)ry-run)"
+	echo "usage: ${script_name} [(l)ink|(u)nlink] (-(-d)ry-run)"
 }
 
 link_opt=
@@ -18,107 +18,119 @@ unlink_opt=
 dryrun_opt=
 
 while [ ${#} -gt 0 ]; do
-    case ${1} in
-        l|link)
-            if [ -n "${unlink_opt}" ]; then
-                { print_usage >&2; exit 1; }
-            else
-                link_opt=1
-            fi
-            ;;
-        u|unlink)
-            if [ -n "${link_opt}" ]; then
-                { print_usage >&2; exit 1; }
-            else
-                unlink_opt=1
-            fi
-            ;;
-        -d|--dry-run)
-            dryrun_opt=1
-            ;;
-        *)
-            { print_usage >&2; exit 1; }
-            ;;
-    esac
+	case ${1} in
+	l | link)
+		if [ -n "${unlink_opt}" ]; then
+			{
+				print_usage >&2
+				exit 1
+			}
+		else
+			link_opt=1
+		fi
+		;;
+	u | unlink)
+		if [ -n "${link_opt}" ]; then
+			{
+				print_usage >&2
+				exit 1
+			}
+		else
+			unlink_opt=1
+		fi
+		;;
+	-d | --dry-run)
+		dryrun_opt=1
+		;;
+	*)
+		{
+			print_usage >&2
+			exit 1
+		}
+		;;
+	esac
 
-    shift
+	shift
 done
 
 target() {
-    echo "${HOME}/.${1#"${script_dir_path}"/}"
+	echo "${HOME}/.${1#"${script_dir_path}"/}"
 }
 
 is_ours() {
-    case $(readlink -f "${1}") in
-        "${script_dir_path}"*) true ;;
-        *) false ;;
-    esac
+	case $(readlink -f "${1}") in
+	"${script_dir_path}"*) true ;;
+	*) false ;;
+	esac
 }
 
 link() {
-    for src in "${1}"/*; do
-        [ "${src}" = "${script_path}" ] && continue
+	for src in "${1}"/*; do
+		[ "${src}" = "${script_path}" ] && continue
 
-        dst=$(target "${src}")
+		dst=$(target "${src}")
 
-        if [ ! -e "${dst}" ]; then
-            if [ -n "${dryrun_opt}" ]; then
-                echo "ln -snv ${src} ${dst}"
-            else
-                ln -snv "${src}" "${dst}"
-            fi
-        elif [ -L "${dst}" ] || [ -f "${dst}" ]; then
-            if is_ours "${dst}"; then
-                echo "${dst} is already linked, skipping" >&2
-            else
-                echo "${dst} already exists, skipping" >&2
-            fi
-        elif [ -d "${dst}" ];then
-            link "${src}"
-        fi
-    done
+		if [ ! -e "${dst}" ]; then
+			if [ -n "${dryrun_opt}" ]; then
+				echo "ln -snv ${src} ${dst}"
+			else
+				ln -snv "${src}" "${dst}"
+			fi
+		elif [ -L "${dst}" ] || [ -f "${dst}" ]; then
+			if is_ours "${dst}"; then
+				echo "${dst} is already linked, skipping" >&2
+			else
+				echo "${dst} already exists, skipping" >&2
+			fi
+		elif [ -d "${dst}" ]; then
+			link "${src}"
+		fi
+	done
 }
 
 unlink() {
-    for src in "${1}"/*; do
-        [ "${src}" = "${script_path}" ] && continue
+	for src in "${1}"/*; do
+		[ "${src}" = "${script_path}" ] && continue
 
-        dst=$(target "${src}")
+		dst=$(target "${src}")
 
-        if [ -L "${dst}" ] && is_ours "${dst}"; then
-            if [ -n "${dryrun_opt}" ]; then
-                echo "rm -v ${dst}"
-            else
-                rm -v "${dst}"
-            fi
-        elif [ -d "${dst}" ]; then
-            unlink "${src}"
-        fi
-    done
+		if [ -L "${dst}" ] && is_ours "${dst}"; then
+			if [ -n "${dryrun_opt}" ]; then
+				echo "rm -v ${dst}"
+			else
+				rm -v "${dst}"
+			fi
+		elif [ -d "${dst}" ]; then
+			unlink "${src}"
+		fi
+	done
 }
 
 if [ -z "${link_opt}" ] && [ -z "${unlink_opt}" ]; then
-    { print_usage >&2; exit 1; }
+	{
+		print_usage >&2
+		exit 1
+	}
 fi
 
 if [ -n "${link_opt}" ]; then
-    if [ ! -e "${HOME}/.config" ]; then
-        echo "Creating ${HOME}/.config..."
-        if [ ! -d "${HOME}/.config" ]; then
-            if [ -n "${dryrun_opt}" ]; then
-                echo "mkdir ${HOME}/.config"
-            else
-                mkdir "${HOME}/.config"
-            fi
-        fi
-    fi
-    echo "Symlinking dotfiles..."
-    link "${script_dir_path}"
+	if [ ! -e "${HOME}/.config" ]; then
+		echo "Creating ${HOME}/.config..."
+		if [ ! -d "${HOME}/.config" ]; then
+			if [ -n "${dryrun_opt}" ]; then
+				echo "mkdir ${HOME}/.config"
+			else
+				mkdir "${HOME}/.config"
+			fi
+		fi
+	fi
+	echo "Symlinking dotfiles..."
+	link "${script_dir_path}"
 fi
 
 if [ -n "${unlink_opt}" ]; then
-    echo "Unlinking dotfiles..."
-    unlink "${script_dir_path}"
+	echo "Unlinking dotfiles..."
+	unlink "${script_dir_path}"
 fi
 
 echo "Done."
